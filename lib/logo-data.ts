31dc9f7089fb7d9dad478e5281538bdfe7b10667
abanero1998-afk <abanero1998@icloud.com>
2026-09-01 02:54:00 +0200

    Add logo data module barrel

---
+++ lib/logo-data.ts
@@ -0,0 +1,4 @@
+import { logoMark } from "./logo-mark";
+import { icon180 } from "./icon-180";
+import { icon192 } from "./icon-192";
+export const IMAGES: Record<string, string> = { logoMark, icon180, icon192 };
